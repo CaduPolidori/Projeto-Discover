7c51a60bad353c3de390b7cf2bad3449c57eaf86
Cadu01~ <cadupolidori@gmail.com>
2023-11-02 14:31:12 -0300

    add new light mode image

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle('light')
+
+const img = document.querySelector("#profile img")
+
+if(html.classList.contains('light')) {
+ img.setAttribute('src', './Assets/avatar-light.png')
+ img.setAttribute('alt', 'Cadu Polidori de touca e casaco nos cânions de Cambará do Sul')
+} else {
+  img.setAttribute('src', './Assets/avatar.png')
+  img.setAttribute('alt', 'Cadu Polidori de pijamas abraçado em uma garrafa de Natasha')
 }
+
+}
+
+
+
+
+

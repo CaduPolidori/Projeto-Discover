dcb2be4ba7bd6195f50149b710c55160fe191c73
Cadu01~ <cadupolidori@gmail.com>
2023-11-02 15:52:17 -0300

    discription of images changed

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const img = document.querySelector("#profile img")
 
 if(html.classList.contains('light')) {
  img.setAttribute('src', './Assets/avatar-light.png')
- img.setAttribute('alt', 'Cadu Polidori de touca e casaco nos cânions de Cambará do Sul')
+ img.setAttribute('alt', 'Cadu Polidori de pijamas abraçado em uma garrafa de Natasha')
 } else {
   img.setAttribute('src', './Assets/avatar.png')
-  img.setAttribute('alt', 'Cadu Polidori de pijamas abraçado em uma garrafa de Natasha')
+  img.setAttribute('alt', 'Cadu Polidori de touca e casaco nos cânions de Cambará do Sul')
 }
 
 }
